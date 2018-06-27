81092e3705ccc3745d5b1828859797b1c203386d
Jerome Czachor <jerome.czachor@g2-inc.com>
2018-06-27 08:17:44 -0400

    Message creator removal from ui

--- webApp/views/__init__.py
+++ webApp/views/__init__.py
@@ -9,7 +9,7 @@ def register_all(app):
     app.logger.info('Registering Endpoint Blueprints')
     app.register_blueprint(root, url_prefix='')
     app.register_blueprint(convert, url_prefix='/convert')
-    app.register_blueprint(create, url_prefix='/create')
+    # app.register_blueprint(create, url_prefix='/create')
     app.register_blueprint(load, url_prefix='/load')
     app.register_blueprint(validate, url_prefix='/validate')
 
