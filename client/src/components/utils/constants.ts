7c4155f334cc3715d5c47fdac025c3e95a1b47fe
Kaitlyn Hsu <kaitlyn.hsu@bestgateeng.com>
2022-12-30 10:10:38 -0500

    Added meta data, Changed page title to convert-schema

--- client/src/components/utils/constants.ts
+++ client/src/components/utils/constants.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/prefer-default-export
 export const NAV_HOME = '/home';
 export const NAV_VALIDATE = '/validate';
-export const NAV_CONVERT = '/convert';
+export const NAV_CONVERT = '/convert-schema';
 export const NAV_GENERATE_MESSAGE= '/generate-message';
 export const NAV_GENERATE_SCHEMA = '/generate-schema';
 
