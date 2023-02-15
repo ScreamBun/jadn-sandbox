febe2485dac9a64e508d82837a437c0d07483582
Matt Roberts <matthew.roberts@bestgateeng.com>
2023-02-15 12:59:00 -0500

    Updated code mirror with light theme

--- client/src/components/common/SBEditor.tsx
+++ client/src/components/common/SBEditor.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import CodeMirror from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
+import { githubLight } from '@uiw/codemirror-themes-all';
 
 const SBEditor = (props: any) => {
+
+    const code = 'const a = 0;';
+
     const onChange = React.useCallback((value, viewUpdate) => {
         console.log('value:', value);
       }, []);
+
+
+    //   new EditorView({
+    //     doc: "console.log('hello')\n",
+    //     extensions: [basicSetup, javascript()],
+    //     parent: document.body
+    //   })
+
       return (
         <CodeMirror
-          value="console.log('hello world!');"
-          height="200px"
-          extensions={[javascript({ jsx: true })]}
-          onChange={onChange}
+            value={code}
+            height="40em"
+            theme={githubLight}        
+            extensions={[javascript({ jsx: true })]}
+            onChange={onChange}
         />
       );
   }
