98f03fc2c90ec0ad542d94fb20797f4952d3faa2
Matt Roberts <matthew.roberts@bestgateeng.com>
2023-04-28 14:40:51 -0400

    adjustments for win

--- client/config/webpack.config.dev.babel.js
+++ client/config/webpack.config.dev.babel.js
@@ -41,7 +41,8 @@ module.exports = merge(baseConfig, {
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:8080',
-        secure: false
+        secure: false,
+        changeOrigin: true
       }
     },
     static: {
--- server/start.sh
+++ server/start.sh
@@ -1,4 +1,4 @@
 export FLASK_APP=webApp
 export FLASK_DEBUG=1
 
-python -m flask run --host localhost --port 8080
+python -m flask run --host 127.0.0.1 --port 8080
