fb1bd0d0f99fb3b545cf968f706c253b7b6071ab
Matt Roberts <matthew.roberts@bestgateeng.com>
2023-05-02 13:30:51 -0400

    Fixed merge conflicts

--- server/webApp/views/api/format.py
+++ server/webApp/views/api/format.py
@@ -3,14 +3,10 @@ import json
 import logging
 import traceback
 
-from flask import current_app, jsonify, request, render_template
+from flask import Blueprint, current_app, jsonify, request, render_template
 from flask_restful import Resource, reqparse
 from jadnschema.convert import dumps
-<<<<<<< HEAD
-from flask import Blueprint
-=======
 from pydantic import ValidationError
->>>>>>> 4772ba959b2b50ee90f42b5cf181d647e8deb8ab
 
 from webApp.validator.utils import getValidationErrorMsg, getValidationErrorPath
 
@@ -20,14 +16,6 @@ parser = reqparse.RequestParser()
 parser.add_argument("schema", type=dict)
 
 class Format(Resource):
-
-    blog = Blueprint('blog', __name__)
-
-    ## as a decorator
-    @blog.errorhandler(500)
-    def internal_server_error(e):
-        return render_template('500.html'), 500
-
     """
     Endpoint for api/format
     """
@@ -39,12 +27,6 @@ class Format(Resource):
         try:
             output = dumps(request_json)
 
-<<<<<<< HEAD
-        except (TypeError):
-            tb = traceback.format_exc()
-            tb2 = self.internal_server_error(tb)
-            raise Exception("Error: " + tb2)
-=======
         except (TypeError, ValueError) as err:
             tb = traceback.format_exc()
             print(tb)
@@ -57,7 +39,6 @@ class Format(Resource):
                 return errorMsgs, 500
             else:
                 return err, 500
->>>>>>> 4772ba959b2b50ee90f42b5cf181d647e8deb8ab
         
         return jsonify({
         "schema": output
