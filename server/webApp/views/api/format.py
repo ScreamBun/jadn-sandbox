ec642b13c05501e232ca533f82427ff38d9f3411
Kouloum Abidji <Kouloum.Abidji@hii-tsd.com>
2023-05-02 12:25:53 -0400

    Changes

--- server/webApp/views/api/format.py
+++ server/webApp/views/api/format.py
@@ -3,9 +3,10 @@ import json
 import logging
 import traceback
 
-from flask import current_app, jsonify, request
+from flask import current_app, jsonify, request, render_template
 from flask_restful import Resource, reqparse
 from jadnschema.convert import dumps
+from flask import Blueprint
 
 
 logger = logging.getLogger(__name__)
@@ -14,6 +15,14 @@ parser = reqparse.RequestParser()
 parser.add_argument("schema", type=dict)
 
 class Format(Resource):
+
+    blog = Blueprint('blog', __name__)
+
+    ## as a decorator
+    @blog.errorhandler(500)
+    def internal_server_error(e):
+        return render_template('500.html'), 500
+
     """
     Endpoint for api/format
     """
@@ -25,12 +34,13 @@ class Format(Resource):
         try:
             output = dumps(request_json)
 
-        except (TypeError, ValueError):
+        except (TypeError):
             tb = traceback.format_exc()
-            raise Exception("Error: " + tb)
+            tb2 = self.internal_server_error(tb)
+            raise Exception("Error: " + tb2)
         
         return jsonify({
-            "schema": output
+        "schema": output
         })
     
 
@@ -48,7 +58,7 @@ class FormatOptions(Resource):
                 "format_options": format_options
             }) 
         except:
-            raise  traceback.print_exc()
+            raise traceback.print_exc()
 
         return j
          
