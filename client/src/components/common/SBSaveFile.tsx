c3d089a5669bacb77d6ed777036c54bdf9be7b7e
Kaitlyn Hsu <kaitlyn.hsu@bestgateeng.com>
2023-08-07 10:47:03 -0400

    update drop down

--- client/src/components/common/SBSaveFile.tsx
+++ client/src/components/common/SBSaveFile.tsx
@@ -5,6 +5,7 @@ import { useDispatch } from "react-redux";
 import { faSave } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
+import { info } from "actions/util";
 
 const SBSaveFile = (props: any) => {
 
@@ -51,6 +52,7 @@ const SBSaveFile = (props: any) => {
                     sbToastSuccess(val.payload);
                     setToggleSaveDialog(false);
                     setDropdown({ label: filename, value: filename });
+                    dispatch(info());
                 })
                 .catch((err) => {
                     sbToastError(`Error: ${err.payload.response}`);
