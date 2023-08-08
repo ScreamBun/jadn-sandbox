7af908300f8c12aba69664643a108116a96ef644
Kaitlyn Hsu <kaitlyn.hsu@bestgateeng.com>
2023-08-08 15:05:50 -0400

    err log

--- client/src/components/utils/general.ts
+++ client/src/components/utils/general.ts
@@ -1,12 +1,12 @@
 // General Utility Functions
 import { objectFromTuple } from './object';
 
- /**
-  * Split and space a camelcased string
-  * @param {string} str - camelcase string to split
-  * @returns {string} - split and spaced string
-  */
-  export const splitCamel = (str: string): string => str.replace(/([A-Z])/g, ' $1').replace(/^./, s => s.toUpperCase());
+/**
+ * Split and space a camelcased string
+ * @param {string} str - camelcase string to split
+ * @returns {string} - split and spaced string
+ */
+export const splitCamel = (str: string): string => str.replace(/([A-Z])/g, ' $1').replace(/^./, s => s.toUpperCase());
 
 /**
  * Create an object using the given arrays
@@ -19,10 +19,11 @@ import { objectFromTuple } from './object';
 export const zip = (keys: Array<string>, values: Array<any>): Record<string, any> => {
   if (keys.length < values.length) {
     // console.log('Zip', keys, values);
+    console.error(keys, values)
     throw new RangeError('The keys arrays should have the same or more values than the value array');
   }
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return objectFromTuple(
-    ...values.map<[string, any]>((v: any, i: number) => [keys[i], v] )
+    ...values.map<[string, any]>((v: any, i: number) => [keys[i], v])
   );
 };
