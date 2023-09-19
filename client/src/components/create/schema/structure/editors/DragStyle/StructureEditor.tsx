90137f4399e7ba572fbecc8e579df9c9ce4a9a36
Kaitlyn Hsu <kaitlyn.hsu@bestgateeng.com>
2023-09-19 15:05:09 -0400

    disabled validation btn fix

--- client/src/components/create/schema/structure/editors/DragStyle/StructureEditor.tsx
+++ client/src/components/create/schema/structure/editors/DragStyle/StructureEditor.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useEffect, useRef, useState } from 'react';
+import React, { memo, useEffect, useState } from 'react';
 import {
   Button, ButtonGroup, Input
 } from 'reactstrap';
--- client/src/components/validator/MessageValidated.tsx
+++ client/src/components/validator/MessageValidated.tsx
@@ -161,7 +161,7 @@ const MessageValidated = (props: any) => {
                     <div className='col-md float-end'>
                         <SBCopyToClipboard buttonId='copyMessage' data={loadedMsg} customClass='float-right' />
                         <SBSaveFile data={loadedMsg} loc={'messages'} customClass={"float-right mr-1"} filename={fileName} ext={msgFormat ? msgFormat.value : 'json'} setDropdown={setSelectedFile} />
-                        {isLoading ? <SBSpinner action={'Validating'} /> : <Button color="success" className={`float-right mr-1 btn-sm ${Object.keys(validSchema).length != 0 && loadedMsg && decodeMsg && msgFormat ? '' : ' disabled'}`} type="submit"
+                        {isLoading ? <SBSpinner action={'Validating'} /> : <Button color="success" className={`float-right mr-1 btn-sm`} disabled={Object.keys(validSchema).length != 0 && loadedMsg && decodeMsg && msgFormat ? false : true} type="submit"
                             title={'Validate the message against the given schema'}>
                             Validate Message
                         </Button>}
