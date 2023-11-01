22b7901624b67548a584d241fa7df82a4405b053
Kaitlyn Hsu <kaitlyn.hsu@bestgateeng.com>
2023-11-01 13:18:57 -0400

    remove unused import

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy, useContext } from 'react';
+import React, { Suspense, lazy } from 'react';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
