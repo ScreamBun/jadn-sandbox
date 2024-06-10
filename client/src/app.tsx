b79e176a106fa0b0f56e4f13942a3470c7052a6f
Matt Roberts <matthew.roberts@bestgateeng.com>
2024-06-10 14:38:34 -0400

    new data translation page created

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -2,16 +2,8 @@ import React, { Suspense, lazy } from 'react';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// import AppLayout from './components/static/appLayout';
-// import Home from 'components/home/home';
-// import { MessageGenerator, SchemaGenerator } from './components/create/creators';
-// import SchemaVisualizer from './components/visualize/SchemaVisualizer'
-// import MessageValidator from './components/validator/MessageValidator';
-// import About from 'components/about/about';
-// import SchemaTranslator from 'components/translate/SchemaTranslator';
-// import ExampleGenerator from 'components/generate/ExampleGenerator';
-// import SchemaTransformer from 'components/transform/SchemaTransformer';
 import SBSpinner from 'components/common/SBSpinner';
+import DataTranslator from 'components/translate/DataTranslator';
 const AppLayout = lazy(() => import('./components/static/appLayout'));
 const Home = lazy(() => import('components/home/home'));
 const MessageGenerator = lazy(() => import('./components/create/message/MessageGenerator'));
@@ -40,7 +32,8 @@ export const App = () => {
               <Route path="create/schema" element={<SchemaGenerator />} />
               <Route path="transform-schema" element={<SchemaTransformer />} />
               <Route path="generate-message" element={<ExampleGenerator />} />
-              <Route path="translate-schema" element={<SchemaTranslator />} />
+              <Route path="translate/translate-schema" element={<SchemaTranslator />} />
+              <Route path="translate/translate-data" element={<DataTranslator />} />
               <Route path="about" element={<About />} />
               <Route path="*" element={<Home />} />
             </Route>
