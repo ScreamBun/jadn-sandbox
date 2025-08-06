c2e8a309412efbe53a7236d0c3ac2934bb121151
Joshua Cohen <jcohen02@umd.edu>
2025-08-06 10:09:23 -0400

    Fix: ArrayOf onChange

--- client/src/components/create/data/lib/field/types/ArrayOf.tsx
+++ client/src/components/create/data/lib/field/types/ArrayOf.tsx
@@ -103,7 +103,7 @@ const ArrayOf = (props: FieldProps) => {
                     </SBToggleBtn>
                 </div>}
                 <div className={`card-body ${toggleField[i] == true ? '' : 'collapse'}`} id={`${i}`}>
-                    <Field key={`${key} ${i+1}`} field={keyField} parent={String(name)} fieldChange={(k) => addKey(entryName, k)} value={keyEntry?.key ?? ""} />
+                    <Field key={`${key} ${i+1}`} field={keyField} parent={String(name)} fieldChange={(n, k) => addKey(entryName, k)} value={keyEntry?.key ?? ""} />
                 </div>
             </div>
         )
