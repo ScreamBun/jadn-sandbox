b82e45128b2ea50200aff2098dbf37f9ff5d5c40
Matt Roberts <matthew.roberts@bestgateeng.com>
2025-08-28 14:33:23 -0400

    removed unused imports and code

--- server/webApp/validator/validator.py
+++ server/webApp/validator/validator.py
@@ -3,13 +3,7 @@ import cbor2
 import jadn 
 import xml.etree.ElementTree as ET
 
-# from flask import current_app
 from typing import Tuple, Union
-
-# from jadnschema.jadn import loads
-# from jadnschema.schema import Schema
-# from jadnschema.convert.message import SerialFormats
-# from jadnschema.convert.schema.writers.json_schema.schema_validator import validate_schema_jadn_syntax
 
 from jadnvalidation import DataValidation
 
@@ -155,9 +149,9 @@ class Validator:
         :return: (tuple) valid/invalid bool, message/schema
         """
         try :
-            j_validation = DataValidation(self.j_meta_schema, "Schema", schema, SerialFormats.JSON)
+            j_validation = DataValidation(self.j_meta_schema, "Schema", schema)
             j_validation.validate()
-            return True, "Validation passed", json.dumps(schema), schema
+            return True, "Validation passed", "", schema
         except Exception as e:
             errorMsgs = []
             errorMsgs = get_value_errors(e)
@@ -173,9 +167,6 @@ class Validator:
         :param decode: format to decode the message as
         :return: valid/invalid bool, message
         """
-        # v, s = self.validateSchema(schema, False)
-        # if not v:
-            # return False, s, "", data
 
         if isinstance(schema, str):
             return False, "Schema Invalid - The schema failed to load", "", data
@@ -183,11 +174,9 @@ class Validator:
         if data_format not in SerialFormats:
             return False, "Serialization Format not found", "", data
         
-        # # TODO: Validation logic should be moved to jadnvalidation \ data validaiton
         try :
             j_validation = DataValidation(self.j_meta_schema, "Schema", schema, SerialFormats.JSON)
             j_validation.validate()
-            # return True, "Validation passed", json.dumps(schema), schema
         except Exception as e:
             errorMsgs = []
             errorMsgs = get_value_errors(e)
@@ -232,35 +221,12 @@ class Validator:
 
 
     def validate_jadn(self, jadn_src: dict) -> Tuple[bool, str]:
-
-        # try: 
-        #     validate_schema_jadn_syntax(jadn_src) 
-        # except Exception as ex:
-        #     print(f"Syntax Error: {str(ex)}")
-        #     return False, f"Schema Error - {ex}"  
-        
-        # valid_bool, err = current_app.validator.validateSchema(jadn_src) 
-        # if not valid_bool:
-        #     print(f"JADN Error: {str(err)} ")
-        #     return False, f"JADN Error - {err}" 
-
-        # try: 
-        #     jadn.check(jadn_src) # uses jsonschema to check jadn - jadn_v1.0_schema.json
-        # except Exception as ex:
-        #     print(f"JADN Error : {ex}")
-        #     return False, f"JADN Error - {ex}"   
-        
         try :
             j_validation = DataValidation(self.j_meta_schema, "Schema", jadn_src, SerialFormats.JSON.value)
             j_validation.validate()
             return True, ""
         except Exception as e:
-            # errorMsgs = []
-            # errorMsgs = get_value_errors(e)
-            # return False, errorMsgs, "", jadn_src  
-            return False, f"Schema Error - {e}"   
-        
-        # return True, ""
+            return False, f"Schema Error - {e}"
     
     def validate_jidl(self, jidl_src: str) -> Tuple[bool, str]:
         
