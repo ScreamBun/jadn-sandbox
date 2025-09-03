4990783ccd64537a7d188a5659e33b342c1dac75
Joshua Cohen <jcohen02@umd.edu>
2025-09-03 14:22:38 -0400

    Just move format up

--- client/src/components/create/schema/structure/editors/options/FieldOptionsEditor.tsx
+++ client/src/components/create/schema/structure/editors/options/FieldOptionsEditor.tsx
@@ -13,56 +13,27 @@ interface FieldOptionsEditorProps {
 const FieldOptionsEditor = memo(function FieldOptionsEditor(props: FieldOptionsEditorProps) {
   const { id, change, deserializedState, fieldOptions } = props;
 
-  // Separate checkbox options from others
-  const checkboxKeys = Object.keys(FieldOptionInputArgs).filter(
-    key => FieldOptionInputArgs[key].type === 'checkbox' || FieldOptionInputArgs[key].type === 'radio'
-  );
-  const otherKeys = Object.keys(FieldOptionInputArgs).filter(
-    key => FieldOptionInputArgs[key].type !== 'checkbox' && FieldOptionInputArgs[key].type !== 'radio'
-  );
-
-  const checkboxOptions = checkboxKeys.map(key => (
-    <div key={key} className="col-md-3">
-      <KeyValueEditor
-        id={id}
-        name={key}
-        {...FieldOptionInputArgs[key]}
-        placeholder={key}
-        removable={false}
-        change={val => change([key, val], 'field')}
-        value={deserializedState[key]}
-        labelColumns={3}
-        fieldColumns={9}
-      />
-    </div>
-  ));
-
-  const otherOptions = otherKeys.map(key => (
-    <div key={key} className="col-md-6">
-      <KeyValueEditor
-        key={key}
-        id={id}
-        name={key}
-        {...FieldOptionInputArgs[key]}
-        placeholder={key}
-        removable={false}
-        change={val => change([key, val], 'field')}
-        value={deserializedState[key]}
-        labelColumns={4}
-        fieldColumns={7}
-      />
-    </div>
-  ));
+  const validOptions = () => {
+    return Object.keys(FieldOptionInputArgs).map((key: string) => {
+      return (
+        <KeyValueEditor
+          key={key}
+          id={id}
+          name={key}
+          {...FieldOptionInputArgs[key]}
+          placeholder={key}
+          removable={false}
+          change={val => change([key, val], 'field')}
+          value={deserializedState[key]}
+        />
+      );
+    });
+  };
 
   if (fieldOptions) {
     return (
       <>
-        <div className="d-flex flex-wrap align-items-center mb-2">
-          {otherOptions}
-        </div>
-        <div className="d-flex flex-wrap align-items-start mb-2">
-          {checkboxOptions}
-        </div>
+        {validOptions()}
       </>
     );
   }
