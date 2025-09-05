4778e7491ba4fa40e922c488d16903d9567c05fb
Matt Roberts <matthew.roberts@bestgateeng.com>
2025-09-05 10:26:33 -0400

    Updated the OSCAL AP and updated the JADN Validation whl with an updated j_meta_schema.

--- server/webApp/validator/validator.py
+++ server/webApp/validator/validator.py
@@ -7,6 +7,7 @@ from typing import Tuple, Union
 
 from jadnschema.jadn import loads
 from jadnvalidation import DataValidation
+from jadnvalidation.data_validation.schemas.jadn_meta_schema import j_meta_schema, j_meta_roots
 
 from webApp.validator.utils import BaseEnum
 from webApp.utils.utils import get_value_errors
@@ -32,117 +33,6 @@ class Validator:
         "Validation error"
     ]
     
-    # TODO: Move to jadnvalidation at the default schema for validations
-    j_meta_schema = {
-        "meta": {
-        "title": "JADN Metaschema",
-        "package": "http://oasis-open.org/openc2/jadn/v2.0/schema",
-        "description": "Syntax of a JSON Abstract Data Notation (JADN) package.",
-        "license": "CC-BY-4.0",
-        "roots": ["Schema"],
-            "config": {
-                "$FieldName": "^[$A-Za-z][_A-Za-z0-9]{0,63}$"
-            }
-        },
-
-        "types": [
-        ["Schema", "Record", [], "Definition of a JADN package", [
-            [1, "meta", "Metadata", ["[0"], "Information about this package"],
-            [2, "types", "Type", ["[1", "]-1"], "Types defined in this package"]
-        ]],
-
-        ["Metadata", "Map", [], "Information about this package", [
-            [1, "package", "Namespace", [], "Unique name/version of this package"],
-            [2, "version", "String", ["{1", "[0"], "Incrementing version within package"],
-            [3, "title", "String", ["{1", "[0"], "Title"],
-            [4, "description", "String", ["{1", "[0"], "Description"],
-            [5, "comment", "String", ["{1", "[0"], "Comment"],
-            [6, "copyright", "String", ["{1", "[0"], "Copyright notice"],
-            [7, "license", "String", ["{1", "[0"], "SPDX licenseId of this package"],
-            [8, "namespaces", "PrefixNS", ["[0", "]-1"], "Referenced packages"],
-            [9, "roots", "TypeName", ["[0", "]-1"], "Roots of the type tree(s) in this package"],
-            [10, "config", "Config", ["[0"], "Configuration variables"],
-            [11, "jadn_version", "Namespace", ["[0"], "JADN Metaschema package"]
-        ]],
-
-        ["PrefixNs", "Array", [], "Prefix corresponding to a namespace IRI", [
-            [1, "prefix", "NSID", [], "Namespace prefix string"],
-            [2, "namespace", "Namespace", [], "Namespace IRI"]
-        ]],
-
-        ["Config", "Map", ["{1"], "Config vars override JADN defaults", [
-            [1, "$MaxBinary", "Integer", ["y1", "[0"], "Package max octets, default = 255"],
-            [2, "$MaxString", "Integer", ["y1", "[0"], "Package max characters, default = 255"],
-            [3, "$MaxElements", "Integer", ["y1", "[0"], "Package max items/properties, default = 255"],
-            [4, "$Sys", "String", ["{1", "}1", "[0"], "System character for TypeName, default = '.'"],
-            [5, "$TypeName", "String", ["/regex", "[0"], "Default = ^[A-Z][-.A-Za-z0-9]{0,63}$"],
-            [6, "$FieldName", "String", ["/regex", "[0"], "Default = ^[a-z][_A-Za-z0-9]{0,63}$"],
-            [7, "$NSID", "String", ["/regex", "[0"], "Default = ^([A-Za-z][A-Za-z0-9]{0,7})?$"]
-        ]],
-
-        ["Namespace", "String", ["/uri"], "Unique name of a package"],
-
-        ["NSID", "String", ["%^([A-Za-z][A-Za-z0-9]{0,7})?$"], "Namespace prefix matching $NSID"],
-
-        ["TypeName", "String", ["%^[A-Z][-.A-Za-z0-9]{0,63}$"], "Name of a logical type"],
-
-        ["FieldName", "String", ["%^[a-z][_A-Za-z0-9]{0,63}$"], "Name of a field in a structured type"],
-
-        ["TypeRef", "String", [], "Reference to a type, matching ($NSID ':')? $TypeName"],
-
-        ["Type", "Array", [], "", [
-            [1, "type_name", "TypeName", [], ""],
-            [2, "core_type", "Enumerated", ["#JADN-Type"], ""],
-            [3, "type_options", "Options", ["[0"], ""],
-            [4, "type_description", "Description", ["[0"], ""],
-            [5, "fields", "JADN-Type", ["[0", "&2"], ""]
-        ]],
-
-        ["JADN-Type", "Choice", [], "", [
-            [1, "Binary", "Empty", [], ""],
-            [2, "Boolean", "Empty", [], ""],
-            [3, "Integer", "Empty", [], ""],
-            [4, "Number", "Empty", [], ""],
-            [5, "String", "Empty", [], ""],
-            [6, "Enumerated", "Items", [], ""],
-            [7, "Choice", "Fields", [], ""],
-            [8, "Array", "Fields", [], ""],
-            [9, "ArrayOf", "Empty", [], ""],
-            [10, "Map", "Fields", [], ""],
-            [11, "MapOf", "Empty", [], ""],
-            [12, "Record", "Fields", [], ""]
-        ]],
-
-        ["Empty", "Array", ["}0"], "", []],
-
-        ["Items", "ArrayOf", ["*Item"], ""],
-
-        ["Fields", "ArrayOf", ["*Field"], ""],
-
-        ["Item", "Array", [], "", [
-            [1, "item_id", "FieldID", [], ""],
-            [2, "item_value", "String", [], ""],
-            [3, "item_description", "Description", ["[0"], ""]
-        ]],
-
-        ["Field", "Array", [], "", [
-            [1, "field_id", "FieldID", [], ""],
-            [2, "field_name", "FieldName", [], ""],
-            [3, "field_type", "TypeRef", [], ""],
-            [4, "field_options", "Options", ["[0"], ""],
-            [5, "field_description", "Description", ["[0"], ""]
-        ]],
-
-        ["FieldID", "Integer", ["y0"], ""],
-
-        ["Options", "ArrayOf", ["*Option"], ""],
-
-        ["Option", "String", ["{1"], ""],
-
-        ["Description", "String", [], ""]
-        ]
-    }  
-
     def validateSchema(self, schema: Union[bytes, dict, str], sm: bool = True) -> Tuple[bool, Union[str, any]]:
         """
         Validate the given schema
@@ -166,6 +56,7 @@ class Validator:
             return False, f"Schema Invalid - {e}"        
         
 
+    # TODO: Change from validateMessage to validateData
     def validateMessage(self, schema: Union[bytes, dict, str], data: Union[str, bytes, dict], data_format: str, root: str) -> Tuple:
         """
         Validate messages against the given schema
@@ -182,8 +73,9 @@ class Validator:
         if data_format not in SerialFormats:
             return False, "Serialization Format not found", "", data
         
+        # Validate the schema first
         try :
-            j_validation = DataValidation(self.j_meta_schema, "Schema", schema, SerialFormats.JSON.value)
+            j_validation = DataValidation(j_meta_schema, j_meta_roots, schema)
             j_validation.validate()
         except Exception as e:
             errorMsgs = []
@@ -218,6 +110,7 @@ class Validator:
             case _:
                 return False, "Unknown format selected", "", data
         
+        # Validate the data against the user's schema
         try :
             j_validation = DataValidation(schema, root, data, data_format)
             j_validation.validate()
@@ -230,7 +123,7 @@ class Validator:
 
     def validate_jadn(self, jadn_src: dict) -> Tuple[bool, str]:
         try :
-            j_validation = DataValidation(self.j_meta_schema, "Schema", jadn_src, SerialFormats.JSON.value)
+            j_validation = DataValidation(j_meta_schema, j_meta_roots, jadn_src)
             j_validation.validate()
             return True, ""
         except Exception as e:
