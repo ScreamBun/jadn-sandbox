ee82e6657f22d4467fb8d5c7a086434c78e8fe99
Matt Roberts <matthew.roberts@bestgateeng.com>
2025-09-09 10:33:56 -0400

    added buildx

--- docker_push.sh
+++ docker_push.sh
@@ -40,15 +40,21 @@ if [ $? -eq 1 ]; then
   docker rmi $PREV_IMG
 fi
 
-# Build and push new image
+
+# Ensure docker buildx is available
+if ! docker buildx version > /dev/null 2>&1; then
+  echo "Docker buildx is not installed or not available. Please install Docker buildx."
+  exit 1
+fi
+
 docker login
-docker build -f Dockerfile -t $NEW_IMG  .
-docker push $NEW_IMG
+# Build and push multi-platform image (linux/amd64 and linux/arm64)
+docker buildx build --platform linux/arm64,linux/amd64 -f Dockerfile -t $NEW_IMG --push .
 
-{
-  docker build -f Dockerfile -t $IMG_LATEST  .
-} || {
-  exit $1
-}
+# {
+#   docker build -f Dockerfile -t $IMG_LATEST  .
+# } || {
+#   exit $1
+# }
 
-docker push $IMG_LATEST
+# docker push $IMG_LATEST
