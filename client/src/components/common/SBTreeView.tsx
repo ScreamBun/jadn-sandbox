34f621c90d8e2721cc5df0d8526b27d7c221c17c
Joshua Cohen <jcohen02@umd.edu>
2025-11-03 14:46:04 -0500

    SBTreeView List appears

--- client/src/components/common/SBTreeView.tsx
+++ client/src/components/common/SBTreeView.tsx
@@ -1,3 +1,4 @@
+import { destructureField } from "components/create/data/lib/utils";
 import React, { useEffect } from "react";
 
 interface SBTreeViewProps {
@@ -19,24 +20,46 @@ const SBTreeView = (props: SBTreeViewProps) => {
     const pathAppend = (obj: any, basePath: string = ''): string[] => {
         let paths: string[] = [];
         for (const entry of Object.entries(obj)) {
-            const name = entry[1]["text"] || "";
-            const children = entry[1]["value"] || null;
-            const currentPath = basePath ? `${basePath}.${name}` : name;
-            if (children && Array.isArray(children)) {
-                for (const child of children) {
-                    if (!paths.includes(child)) {
-                        console.log(paths);
-                        paths = paths.concat(pathAppend(child, currentPath));
-                    }
+            const field = entry[1]["value"] || null;
+            const [_idx, _name, _type, _options, _comment, _children] = field ? destructureField(field) : [undefined, undefined, undefined, undefined, undefined, undefined];
+            const currentPath = basePath ? `${basePath}.${_name}` : _name;
+            if (field && currentPath) paths.push(currentPath);
+            if (_children && Array.isArray(_children)) {
+                for (const child of _children) {
+                    const [__idx, __name] = destructureField(child);
+                    paths = paths.concat(pathAppend([{text: __name, value: child}], currentPath));
                 }
             }
         }
         return paths;
     };
 
+    // Pull out name
+    const getName = (path: string) => {
+        const parts = path.split('.');
+        return parts[parts.length - 1];
+    }
+
+    // Count how many dots there are in path
+    const countDots = (path: string) => {
+        return path.split('.').length - 1;
+    }
+
+    // Convert paths to cards
+    const pathCards = (paths: string[]) => {
+        return paths.map((path, index) => (
+            <div key={index} className={`card ms-${countDots(path) * 2} p-2 border border-gray-300 rounded`}>
+                {getName(path)}
+            </div>
+        ));
+    }
+
+    // Generate paths and cards
+    const cards = pathCards(pathAppend(schema));
+
     return (
         <p>
-            {pathAppend(schema)}
+            {cards}
         </p>
     );
 }
